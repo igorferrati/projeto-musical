a493441c4fdf6ce68975f1b4a959c426f39e11d7
igorferrati <ferrati.igor@gmail.com>
2024-08-27 22:55:56 -0300

    wip

--- Program.cs
+++ Program.cs
@@ -21,6 +21,22 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite [3] avaliar banda banda");
     Console.WriteLine("Digite [4] média de avaliações de banda");
     Console.WriteLine("Digite [-1] sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    switch (opcaoEscolhidaNumerica) {
+        case 1: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+        case 2: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+        case 3: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+        case 4: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+        case -1: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+    }
 }
 
 ExibirMensagemDeBoasVindas();
