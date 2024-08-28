7e7cfcfdb363d3dbd1070d36a25aee6626e3fb9f
igorferrati <ferrati.igor@gmail.com>
2024-08-27 22:44:37 -0300

    wip

--- Program.cs
+++ Program.cs
@@ -3,10 +3,25 @@ string mensagemDeBoasVindas = "Boas vindas ao Scrren Sound";
 
 void ExibirMensagemDeBoasVindas()
 {
-    Console.WriteLine("****************************");
+    Console.WriteLine(@"
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━━━╮╱╱╱╱╱╱╱╱╱╱╭╮
+┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃╭━╮┃╱╱╱╱╱╱╱╱╱╱┃┃
+┃╰━━┳━━┳━┳━━┳━━┳━╮╱┃╰━━┳━━┳╮╭┳━╮╭━╯┃
+╰━━╮┃╭━┫╭┫┃━┫┃━┫╭╮╮╰━━╮┃╭╮┃┃┃┃╭╮┫╭╮┃
+┃╰━╯┃╰━┫┃┃┃━┫┃━┫┃┃┃┃╰━╯┃╰╯┃╰╯┃┃┃┃╰╯┃
+╰━━━┻━━┻╯╰━━┻━━┻╯╰╯╰━━━┻━━┻━━┻╯╰┻━━╯
+    ");
     Console.WriteLine(mensagemDeBoasVindas);
-    Console.WriteLine("****************************");
+}
 
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite [1] registrar uma banda");
+    Console.WriteLine("Digite [2] mostrar todas banda");
+    Console.WriteLine("Digite [3] avaliar banda banda");
+    Console.WriteLine("Digite [4] média de avaliações de banda");
+    Console.WriteLine("Digite [-1] sair");
 }
 
 ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
