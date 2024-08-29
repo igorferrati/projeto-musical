1aa6b4e56ca61ac5152b2e5582cd282cb55b3072
igorferrati <ferrati.igor@gmail.com>
2024-08-28 21:30:25 -0300

    wip

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿// Screen Sound
+using System.Diagnostics.CodeAnalysis;
+
 string mensagemDeBoasVindas = "Boas vindas ao Scrren Sound";
 // List<string> listaDasBandas = new List<string>{"U2", "The Beatles", "Slipknot"};
 
 Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("Link Park", new List<int> {10, 9, 10});
+bandasRegistradas.Add("Korn", new List<int> {8, 9, 10});
 
 void ExibirLogo()
 {
@@ -34,7 +38,7 @@ void ExibirOpcoesDoMenu()
             break;
         case 2: MostrarBandasRegistradas();
             break;
-        case 3: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+        case 3: AvaliarUmaBanda();
             break;
         case 4: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
             break;
@@ -85,6 +89,32 @@ void ExbirTituloDaOpcao(string titulo)
     Console.WriteLine(titulo);
     Console.WriteLine(asterisco + "\n");
 
+}
+
+void AvaliarUmaBanda()
+{
+    Console.Clear();
+    ExbirTituloDaOpcao("Avaliar banda");
+    Console.Write("Digite o nome da banda que deseja avaliar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        Console.Write($"Digite a nota para {nomeDaBanda}: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeDaBanda].Add(nota);
+        Console.WriteLine("\nNota registrada com sucesso!");
+        Thread.Sleep(2000);
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    } else
+    {
+        Console.WriteLine($"\n A banda: {nomeDaBanda} não foi encontrada!");
+        Console.Write("Digite qualquer tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
 }
 
 ExibirOpcoesDoMenu();
